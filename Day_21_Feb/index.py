9351fb928d8fde8333e69ac8b478cbbbdcdc9063
Nischal <baniyanisal12@gmail.com>
2024-02-21 21:12:29 +0545

    for and while loops in array

--- Day_21_Feb/index.py
+++ Day_21_Feb/index.py
@@ -21,3 +21,33 @@ print(f"The object of the student {DataObject}")
 #Tuple
 DataTuple = (123, 'Python', True)
 print(f"Tuple in python {DataTuple}")
+
+#loops
+Mobile = [{
+    'name': 'Iphone',
+    'price': 120000
+},
+{
+    'name': 'Samsung',
+    'price': 50000
+},
+{
+    'name': 'Red MI',
+    'price': 25000
+},
+{
+    'name': 'Oppo',
+    'price': 20000
+}]
+
+print(f"length of Array {len(Mobile)}")
+
+for i in Mobile:
+    print(f"Mobile Brand: {i['name']} Price: {i['price']}")
+
+i = 0
+
+while(i<len(Mobile)):
+    print(Mobile[i])
+    i = i + 1
+
