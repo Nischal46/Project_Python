ef9a75798eedba3350ceda0e961348dff5b034a8
Nischal <baniyanisal12@gmail.com>
2024-02-21 20:51:55 +0545

    Data Types of python List, Dictionary, Tuple

---
+++ Day_21_Feb/index.py
@@ -0,0 +1,23 @@
+username = 'Nischal'
+print(username)
+
+#List or Array
+
+DataList = ['Nepal', 'America', 'Australia']
+
+print(f"The array of python {DataList}")
+print(f"The array length of python {len(DataList)}")
+
+
+#Object or Dictionary
+DataObject = {'id': '123',
+    'name': 'Nischal Baniya',
+    'email': 'baniyanisal12@gmail.com',
+    'password': 'admin'
+}
+
+print(f"The object of the student {DataObject}")
+
+#Tuple
+DataTuple = (123, 'Python', True)
+print(f"Tuple in python {DataTuple}")
