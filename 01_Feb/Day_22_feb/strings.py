ee9d20ddf7369e4de83b4875742c9337c3b7dc4d
Nischal <baniyanisal12@gmail.com>
2024-02-22 14:00:15 +0545

    string and built instring method

---
+++ 01_Feb/Day_22_feb/strings.py
@@ -0,0 +1,39 @@
+name = 'Nischal Baniya'
+
+#to print the index of the string range from
+print(name[0:7])
+
+#to replace the content of the string
+print(name.replace("Nischal", "Bibek"))
+
+#to find the length of the string
+print(len(name)) # basically this is used to fill the name that must not be greater than for example 15 character
+
+newstring = '  hello programmer '
+print(newstring)
+#to eliminate the unusual space from the user input
+print(newstring.strip())
+
+#to split the string to array for example split my first and last name and arrange in list
+print(name.split(" "))
+
+repeatedString ='Congratulations Developer Developer'
+#to count the repetance of the word
+print(repeatedString.count("Developer"))
+
+#print the letter of the string individually
+
+stack = 'full stack developer'
+
+# for letter in stack:
+#     print(letter)
+
+#print the string that contains the exclamation marks
+    
+quotedstring = "He says \"he is a programmer.\""
+print(quotedstring)
+
+
+#r doesnot break the line r means rawstring
+pathstring = r'c:\dev\data\folder'
+print(pathstring)
