2778b2ebb88b386539b7ecc354cdb8f618744e32
Nischal <baniyanisal12@gmail.com>
2024-02-24 17:09:46 +0545

    include setter method defined in the parent class and it can be access to child class

--- 01_Feb/Day_24_Feb/classandobject.py
+++ 01_Feb/Day_24_Feb/classandobject.py
@@ -4,6 +4,7 @@ class Student:
         self.name = name
         self.email = email
         self.__stCode = Code
+     
 
     def student_Code(self):
         return {'code': self.__stCode}
@@ -11,8 +12,19 @@ class Student:
     def student_details(self):
         return {'name': self.name, 'email': self.email}
     
+    def set_Code(self, Code):
+        print('code in setter', Code)
+        if "@" in Code:
+            self.__stCode = Code
+            return self.__stCode
+        else:
+            print('must include @')
+
+        # use getmethod for giving private variable acess to other child class
     def get_Code(self):
         return self.__stCode + " From getter setter"
+    
+        
     
 
 #Inheritance
@@ -31,9 +43,9 @@ class Faculty_Enroll(Student):
 
 student1 = Student('Nischal', 'nisal@gmail.com', 'we245gg')
 print(student1.student_details())
-print(student1.student_Code())
+# print(student1.student_Code())
 print(student1.get_Code())
 
-it_student = Faculty_Enroll('Samir', 'samir@gmail.com', 'MIT', 'fsdfd')
-print(it_student.student_details())
-print(it_student.get_Code())
+# it_student = Faculty_Enroll('Samir', 'samir@gmail.com', 'MIT', 'fsdfd')
+# print(it_student.student_details())
+# print(it_student.get_Code())
