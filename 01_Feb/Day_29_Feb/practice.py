fd26c147c73ae7f9d000aa2207df99690e5a34d2
Nischal <baniyanisal12@gmail.com>
2024-03-10 20:05:18 +0545

    reversing number such as 123 to 321 in int dataatype using while loop

--- 01_Feb/Day_29_Feb/practice.py
+++ 01_Feb/Day_29_Feb/practice.py
@@ -1,11 +1,26 @@
 #reversing thee string
 
-string_original = 'Code everyday'
-string_length = len(string_original)-1
-string_reverse = ""
+string_original = 'Code everyday' #string to be reversed
+# string_length = len(string_original)-1 #determine the length of the string
+# string_reverse = "" #declare the empty string first
 
-while(string_length >= 0):
-    string_reverse = string_reverse + string_original[string_length]
-    string_length = string_length - 1
+# while(string_length >= 0):
+#     string_reverse = string_reverse + string_original[string_length]
+#     string_length = string_length - 1
 
-print(string_reverse)
+# print(string_reverse)
+
+number = 3456 # '3456'
+
+numtostr = str(number)
+numlength = len(numtostr) - 1
+numreverse = ''
+
+while(numlength >= 0):
+    numreverse = numreverse + numtostr[numlength]
+    numlength = numlength - 1
+
+finalreverse = int(numreverse)
+print(finalreverse, type(finalreverse))
+
+# print(numtostr[1])
