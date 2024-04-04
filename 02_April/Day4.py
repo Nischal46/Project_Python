c6208be50f4ee58c0bb7321305d7051a6208728c
Nischal <baniyanisal12@gmail.com>
2024-04-04 22:45:18 +0545

    adding data in array in nth place

--- 02_April/Day4.py
+++ 02_April/Day4.py
@@ -7,10 +7,24 @@ reverse_string = ""
 
 while(string_length >= 0):
     reverse_string = reverse_string + original_string[string_length]
-    
+
     string_length -= 1
 
 if(original_string == reverse_string):
     print(f'{original_string} is a palindrome string')
 else:
     print(f'{original_string} is not palindrome number')
+
+
+
+array = [12,43,54,58,98,66]
+swap = 3
+
+array.append(None)  # Add an extra element at the end to avoid losing the last element
+
+for i in range(len(array) - 1, swap, -1):
+    array[i] = array[i - 1]
+
+array[swap] = 10
+
+print(f'After adding data: {array}')
