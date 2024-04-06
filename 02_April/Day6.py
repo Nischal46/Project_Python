89bf80df8ee6fad3f7d475186d624700afd56769
Nischal <baniyanisal12@gmail.com>
2024-04-06 21:30:15 +0545

    Class and object, constructor function and also inheritence of the multiple class

---
+++ 02_April/Day6.py
@@ -0,0 +1,21 @@
+class Area:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
+    def details(self):
+        print(f'The details of the rectangle are: length {self.length} cm, breadth: {self.breadth} cm')
+
+#inheritence from the parent class
+class Rectangle(Area):
+    def __init__(self, length, breadth):
+        super().__init__(length, breadth)
+
+    def Area_of_Rectangle(self):
+        area = self.length * self.breadth
+        print(f'Area of rectangle is {area} cm^2')
+
+obj1 = Rectangle(3,2)
+obj1.Area_of_Rectangle()
+obj1.details()
+
