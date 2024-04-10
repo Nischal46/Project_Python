5c051c0827915e4c4b2e2115d80b1a92f14b5007
Nischal <baniyanisal12@gmail.com>
2024-04-10 22:57:08 +0545

    implements the inheritence concept and derived from the parent class function and used in child class

--- 02_April/Day10.py
+++ 02_April/Day10.py
@@ -8,5 +8,17 @@ class Songs:
     def song_detail(self):
         print(f'Song Name: {self.song_name}\nOrigin: {self.origin}')
 
-obj1 = Songs('samadhi - 5:55', 'Nepal')
-obj1.song_detail()
+
+class Song_Characteristics(Songs):
+    def __init__(self, song_name, origin, type, singer):
+        super().__init__(song_name, origin)
+        self.type = type
+        self.singer = singer
+
+    def song_characteristecs(self):
+        super().song_detail()
+
+
+
+obj1 = Song_Characteristics('samadhi - 5:55', 'Nepal', 'Pop', 'Chirag Khadka')
+obj1.song_characteristecs()
