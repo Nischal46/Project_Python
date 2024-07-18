dd98517385fa12b82fdbe2fd9626359e476715b6
Nischal <baniyanisal12@gmail.com>
2024-07-18 22:49:50 +0545

    class and object

--- 03_July/Day18.py
+++ 03_July/Day18.py
@@ -3,6 +3,8 @@
 
 # lambda arguements: expression
 
+import re
+
 #first case
 area_of_circle = lambda x, y: 2*x + 2*y
 print(area_of_circle(2,3))
@@ -34,3 +36,28 @@ for x in instrruments_price:
 # we can also get the direct values
 for x in instrruments_price.values():
     print(f'accessing the direct val {x}')
+
+
+
+#class and objects
+
+class Programming:
+    def __init__(self, name, frameworks):
+        self.name = name
+        self.frameworks = frameworks
+
+P1 = Programming('js', ['react', 'angular', 'vue', 'next', 'remix'])
+print(P1.name)
+print(P1.frameworks)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f'{self.name, self.age}'
+
+call = Person("John", 36)
+
+print(call)
