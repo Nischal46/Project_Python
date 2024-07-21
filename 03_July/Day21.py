1e4dbf2ce6dfaaa90f6338f0c9c9027c82595829
Nischal <baniyanisal12@gmail.com>
2024-07-21 23:10:43 +0545

    Practicing python

---
+++ 03_July/Day21.py
@@ -0,0 +1,19 @@
+
+#list
+
+arraylist = ['vanilla js', 'react', 'angular', 'vue', 'next']
+
+#for loop
+
+for iterator in arraylist:
+    print(iterator)
+
+#if else
+
+print('Yes it contains') if 'spring' in arraylist else print('No it does not contains')
+
+#lambda function which is also known as anonymous function
+
+ananymousfn = lambda x: print('lambda function contains ', x)
+ananymousfn(arraylist)
+
