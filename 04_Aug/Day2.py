ef70d52d8bc5549dacb9d0fffdb9ef50286f1b2f
Nischal <baniyanisal12@gmail.com>
2024-08-02 23:38:27 +0545

    try catch function and if else

--- 04_Aug/Day2.py
+++ 04_Aug/Day2.py
@@ -8,3 +8,16 @@ print(greeting('nischal'))
 
 for i in range(0, 12):
     print(f'{i} Hello Friend')
+
+
+def divide(a, b):
+    try:
+        if b == 0:
+            return "Cannot divide by zero"
+        else:
+            return a / b
+    except TypeError:
+        return "Invalid input types"
+
+result = divide(10, 0)
+print(result)
